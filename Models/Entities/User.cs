e16cd0f824019bc3e902e9cbaf35c21c4b13f7c0
wellington silva <wellingtonbento1042@gmail.com>
2026-08-20 22:09:44 -0300

    create models, DbContext, migração inicial

---
+++ Models/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace MyPasswords.Models.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPasswords.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "User name is required.")]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
