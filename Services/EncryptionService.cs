580f330ec6d1c3f854204595fd0f393063f95af4
wellington silva <wellingtonbento1042@gmail.com>
2026-08-24 18:51:38 -0300

    feat: creating PasswordHashService and EncryptionService

---
+++ Services/EncryptionService.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MyPasswords.Services
+{
+    public class EncryptionService
+    {
+        private readonly byte[] _key;
+        public EncryptionService(IConfiguration configuration)
+        {
+            string keyString = configuration["EncryptionKey"];
+            _key = Encoding.UTF8.GetBytes(keyString);
+        }
+
+        public string Encrypt(string text)
+        {
+            using var aes = Aes.Create();
+            aes.Key = _key;
+
+            using var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+            using var memorySream = new MemoryStream();
+            using (var cryptoStream = new CryptoStream(memorySream, encryptor, CryptoStreamMode.Write))
+            using (var streamWriter = new StreamWriter(cryptoStream))
+            {
+                streamWriter.Write(text);
+            }
+
+            byte[] iv = aes.IV;
+            byte[] ciphertext = memorySream.ToArray();
+            byte[] result = iv.Concat(ciphertext).ToArray();
+
+            return Convert.ToBase64String(result);
+        }
+
+        public string Decrypt(string encryptedText)
+        {
+            byte[] bytes = Convert.FromBase64String(encryptedText);
+
+            byte[] iv = bytes.Take(16).ToArray();           
+            byte[] ciphertext = bytes.Skip(16).ToArray();
+
+            using var aes = Aes.Create();
+            aes.Key = _key;
+            aes.IV = iv;
+
+            using var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+            using var memoryStream = new MemoryStream(ciphertext);
+            using var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            using var streamWriter = new StreamReader(cryptoStream);
+
+            return streamWriter.ReadToEnd();
+        }
+    }
+}
---
+++ Services/PasswordHashService.cs
@@ -0,0 +1,8 @@
+﻿namespace MyPasswords.Services
+{
+    public class PasswordHashService
+    {
+        public string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+        public bool VerifyPassword(string password, string hash) => BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
