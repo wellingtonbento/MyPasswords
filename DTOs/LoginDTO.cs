ef68bf6e954aed631b9bd5f076972767c56861ac
wellington silva <wellingtonbento1042@gmail.com>
2026-09-03 15:01:14 -0300

    add cookie-based authentication with login flow

---
+++ DTOs/LoginDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPasswords.DTOs
+{
+    public class LoginDTO
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
---
+++ Services/Account/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿using MyPasswords.DTOs;
+using MyPasswords.Models;
+
+namespace MyPasswords.Services.Account
+{
+    public interface IAccountService
+    {
+        Task<LoginDTO?> Login(LoginViewModel model);
+    }
+}
